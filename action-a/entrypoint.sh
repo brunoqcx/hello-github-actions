01c5b7ab4bdb9acfd042eb033909e37729bcfa71
Bruno <brunotavm@gmail.com>
2019-12-12 11:41:17 -0300

    change text

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 sh -c "echo Hello world my name is $MY_NAME"
-curl -X POST -H 'Content-type: application/json' --data '{"text":"fez um novo pull request"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"$MY_NAME criou um novo PR"}' https://hooks.slack.com/services/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
