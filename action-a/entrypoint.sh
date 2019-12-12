897eaab388e771efdb1f973b8fb3831a3029f37b
Bruno <brunotavm@gmail.com>
2019-12-12 13:44:43 -0300

    change secretsss

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 sh -c "echo Hello world my name is $MY_NAME"
-curl -X POST -H 'Content-type: application/json' --data '{"text":"$MY_NAME criou um novo PR"}' ${{ secrets.slackWebhook }}
+curl -X POST -H 'Content-type: application/json' --data '{"text":"$MY_NAME criou um novo PR"}' $SLACK_WEBHOOK
